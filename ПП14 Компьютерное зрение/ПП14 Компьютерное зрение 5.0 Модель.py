3700ee2671ee6d15b24dd864e1b09c02805230d3
Krutikov Dmitry <dimotey@yandex.ru>
2021-01-31 19:06:51 +0300

    all nootbook

---
+++ ПП14 Компьютерное зрение/ПП14 Компьютерное зрение 5.0 Модель.py
@@ -0,0 +1,83 @@
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.resnet import ResNet50
+import pandas as pd
+
+def load_train(path):
+    labels = pd.read_csv(path + '/labels.csv')
+    train_datagen = ImageDataGenerator(validation_split=0.25,# ДОБАВЛЕНА ВАЛИДАЦИЯ В ОБУЧАЮЩИЙ ImageDataGenerator
+                                       rescale=1./255,
+                                       horizontal_flip=True,
+                                       #vertical_flip=True, # УБРАН ВЕРТИКАЛЬНЫЙ ФЛИП
+                                       #rotation_range=90,
+                                       #width_shift_range=0.2,
+                                       #height_shift_range=0.2
+                                       )
+    
+    train_datagen_flow = train_datagen.flow_from_dataframe(dataframe=labels,
+                                                          directory= path + '/final_files/',
+                                                          x_col='file_name',
+                                                          y_col='real_age',
+                                                          target_size=(224, 224), 
+                                                          batch_size=32,
+                                                          class_mode='raw',
+                                                          #subset='training',
+                                                          seed=12345        )
+    return train_datagen_flow
+
+def load_test(path):
+    labels = pd.read_csv(path + '/labels.csv')
+    test_datagen = ImageDataGenerator(
+                                       rescale=1./255,
+                                       #horizontal_flip=True, # УДАЛЕНА АУГМЕНТАЦИЯ В ДЛЯ ТЕСТОВОЙ ВЫБОРКИ
+                                       #vertical_flip=True,
+                                       #rotation_range=90,
+                                       #width_shift_range=0.2,
+                                       #height_shift_range=0.2
+                                       )
+    
+    test_datagen_flow = test_datagen.flow_from_dataframe(
+        dataframe=labels,
+        directory= path + '/final_files/',
+        x_col='file_name',
+        y_col='real_age',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='raw',
+        #subset='testing',
+        seed=12345
+        )
+
+    return test_datagen_flow
+def create_model(input_shape):
+    optimizer = Adam(lr=0.0001)#0.0001
+    backbone = ResNet50(input_shape=input_shape,
+                    weights='imagenet', 
+                    include_top=False)
+
+    model = Sequential()
+    model.add(backbone)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(1, activation='relu')) 
+    model.compile(optimizer=optimizer,
+                  loss='mse',
+                  metrics=['mae'])
+    
+    return model
+
+def train_model(model, train_data, test_data, batch_size=None, epochs=10,
+               steps_per_epoch=None, validation_steps=None):
+    if steps_per_epoch is None:
+        steps_per_epoch = len(train_data)
+    if validation_steps is None:
+        validation_steps = len(test_data)
+    model.fit(train_data,
+          validation_data=test_data,
+          steps_per_epoch=steps_per_epoch,
+          validation_steps=validation_steps,
+          batch_size=batch_size,
+          verbose=2, epochs=epochs)
+
+    return model 
